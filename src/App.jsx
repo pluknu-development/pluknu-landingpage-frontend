61f5226909dbad67a82619cc93e48b68a68a3cfa
Harmen Meinema <harmen@mooore.nl>
2026-03-05 10:46:10 +0100

    KPSO-87 ADD: Setting up the development environment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1 className='font-heading text-4xl'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
